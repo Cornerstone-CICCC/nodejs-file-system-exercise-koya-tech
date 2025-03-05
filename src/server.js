45179404e13f244a9bfe39281d144931e6e3f981
koya <koya9431ptr@gmail.com>
2025-03-05 20:03:46 +0000

    done

--- src/server.js
+++ src/server.js
@@ -1,32 +1,32 @@
-import http from 'http';
-import fs from 'fs';
-import path from 'path';
-
-const port = 3000;
-
-const server = http.createServer((req, res) => {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var http_1 = require("http");
+var fs_1 = require("fs");
+var path_1 = require("path");
+var port = 3000;
+var server = http_1.default.createServer(function (req, res) {
     if (req.url === '/view-image' && req.method === 'GET') {
         // Construct the absolute path to veryhappydog.jpg assuming it's in the project root directory
-        const imagePath = path.join(__dirname, '../dist/images', 'veryhappydog.jpg');
-        
-        fs.readFile(imagePath, (err, data) => {
+        var imagePath = path_1.default.join(__dirname, '../dist/images', 'veryhappydog.jpg');
+        fs_1.default.readFile(imagePath, function (err, data) {
             if (err) {
                 res.statusCode = 500;
                 res.setHeader('Content-Type', 'text/plain');
                 res.end('Error reading the image file.');
-            } else {
+            }
+            else {
                 res.statusCode = 200;
                 res.setHeader('Content-Type', 'image/jpeg');
                 res.end(data);
             }
         });
-    } else {
+    }
+    else {
         res.statusCode = 404;
         res.setHeader('Content-Type', 'text/plain');
         res.end('Not Found');
     }
 });
-
-server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+server.listen(port, function () {
+    console.log("Server is running on http://localhost:".concat(port));
 });
